68a581b50c3665255668c63c8962e33ffcfdd4ca
Reyfalino <ammarzaidan36@gmail.com>
2024-01-08 13:46:34 +0700

    Laravel CRUD Tweet

---
+++ app/Http/Controllers/Tweet/TweetEditController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Models\Tweet;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TweetEditController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke($id): View
+    {
+        $tweet = Tweet::find($id);
+
+       $this->authorize('update', $tweet);
+
+        return view('tweets.edit', [
+            'tweet' => $tweet,
+        ]);
+    }
+}
---
+++ resources/views/timeline.blade.php
@@ -0,0 +1,45 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="card bg-white">
+                <div class="card-body">
+                    <form action="{{ route('tweets.store') }}" method="post">
+                            @csrf
+                        <textarea name="content" class="textarea textarea-bordered w-full" placeholder="Apa Yang Kamu Pikirkan?" rows="3"></textarea>
+                        <input type="submit" value="Tweet" class="btn btn-secondary">
+                    </form>
+                </div>
+            </div> 
+            
+            @foreach ($tweets as $tweet)
+            <div class="card my-4 bg-white">
+                <div class="card-body">
+                    <h2 class="text-xl font-bold">{{ $tweet->user->name }}</h2>
+                    <p>{{ $tweet->content}}</p>
+                    <div class="text-end">
+                        @can('update', $tweet)
+                        <a href="{{ route('tweets.edit', $tweet->id) }}"
+                         class="link link-hover text-blue-400 btn btn-outline btn-primary btn-xs"> Edit 
+                        </a>
+                        @endcan
+                        @can('delete', $tweet)
+                        <form action="{{ route('tweets.destroy', $tweet->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-error btn-sm">DELETE</button>
+                        </form>
+                        @endcan
+                        <span class="badge">{{ $tweet->created_at->diffForHumans() }}</span>
+                    </div>
+                </div>
+            </div>
+                @endforeach
+        </div>
+    </div>
+</x-app-layout>
